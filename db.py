39611b4a0483b0a093b06c7a75b5e588a63a5b5c
Miffy Cheng <chiao.cheng22@gmail.com>
2025-05-02 03:22:28 -0500

    Updated files

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # MongoDB Atlas connection string
-client = MongoClient("mongodb+srv://chiaocheng22:jTF7Q90sqSJuaFbS@cluster0.ulqgakk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("USE YOUR OWN STRING")
 db = client.IS455Final  # Reference to the 'IS455Final' database
 
 # Export db for use in app.py
